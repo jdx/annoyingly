40a4fefa851c169c05a205df565dffd09ca0ca8d
dickeyxxx <jeff@dickey.xxx>
2012-06-23 12:01:32 -0700

    added inital application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Annoyingly::Application.config.secret_token = 'fd93550fdc8150064470466113d14b7be69a13133d3dee988f56b2f54ab0fe5791556ac9ed827f8fdb7f1428cddec00be17b3404964b7d82db4cea6ea9491fd6'
